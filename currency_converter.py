6acdeb320c1659a37eeb5c03d52e6fca7dd8a700
Mariano Vazquez <mariano.vazquez@live.com>
2019-09-28 00:28:59 +0100

    first iteration complete

---
+++ currency_converter.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+
+#Options for Local box
+chrome_options = webdriver.ChromeOptions()
+chrome_driver_path = '/Users/mariano/Desktop/scrapers/chromedriver'
+driver = webdriver.Chrome(chrome_driver_path, chrome_options=chrome_options)
+
+url = 'https://transferwise.com/ie/currency-converter/'
+
+driver.get(url)
+wait = WebDriverWait(driver, 10)
+amount = wait.until(EC.visibility_of_element_located((By.ID, "cc-amount")))
+amount.click()
+amount.send_keys("1500")
+
+select_source = Select(driver.find_element_by_id('sourceCurrency'))
+select_source.select_by_value('USD')
+
+
+select_target = Select(driver.find_element_by_id('targetCurrency'))
+select_target.select_by_value('GBP')
+
+convert_btn = driver.find_element(By.ID,"convert").click()
+
+
+converted_currency = wait.until(EC.visibility_of_element_located((By.ID, "cc-amount-to")))
+
+converted_amount = driver.find_element_by_id("cc-amount-to").get_attribute('value')
+
+
+
+print(converted_amount)
