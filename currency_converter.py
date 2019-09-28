0bd22689380ee7174ffcfef075d764a60704ed7f
Mariano Vazquez <mariano.vazquez@live.com>
2019-09-28 01:11:28 +0100

    Added loop to iterate over different currencies

--- currency_converter.py
+++ currency_converter.py
@@ -6,6 +6,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
+import os
+import random
 
 #Options for Local box
 chrome_options = webdriver.ChromeOptions()
@@ -14,26 +16,42 @@ driver = webdriver.Chrome(chrome_driver_path, chrome_options=chrome_options)
 
 url = 'https://transferwise.com/ie/currency-converter/'
 
-driver.get(url)
-wait = WebDriverWait(driver, 10)
-amount = wait.until(EC.visibility_of_element_located((By.ID, "cc-amount")))
-amount.click()
-amount.send_keys("1500")
+def currency_iteration(source_amount, url):
 
-select_source = Select(driver.find_element_by_id('sourceCurrency'))
-select_source.select_by_value('USD')
+    driver.get(url)
+
+    source_c = ['USD', 'GBP', 'ARS', 'EUR', 'CAD']
+    target_c = ['INR', 'AUD', 'MXN', 'JPY', 'NZD']
+
+    for source in source_c:
+        for target in target_c:
+            wait = WebDriverWait(driver, 10)
+            amount = wait.until(EC.visibility_of_element_located((By.ID, "cc-amount")))
+            amount.click()
+            amount.send_keys(str(source_amount))
+
+            select_source = Select(driver.find_element_by_id('sourceCurrency'))
+            select_source.select_by_value(source)
+
+            select_target = Select(driver.find_element_by_id('targetCurrency'))
+            select_target.select_by_value(target)
+
+            convert_btn = driver.find_element(By.ID,"convert").click()
+
+            converted_currency = wait.until(EC.visibility_of_element_located((By.ID, "cc-amount-to")))
+
+            converted_amount = driver.find_element_by_id("cc-amount-to").get_attribute('value')
+
+            print(str(source_amount) + " " + source + " are equal to " + str(converted_amount) + " " + target)
+
+            driver.back()
 
 
-select_target = Select(driver.find_element_by_id('targetCurrency'))
-select_target.select_by_value('GBP')
-
-convert_btn = driver.find_element(By.ID,"convert").click()
+if __name__ == '__main__':
 
 
-converted_currency = wait.until(EC.visibility_of_element_located((By.ID, "cc-amount-to")))
+  os.system('clear')
 
-converted_amount = driver.find_element_by_id("cc-amount-to").get_attribute('value')
+  source_amount = input("Enter the amount to calculate: ")
 
-
-
-print(converted_amount)
+  currency_iteration(source_amount, url)
