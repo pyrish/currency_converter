d9bf267c3aaf4452bc49a4c0a332a5dc1a82e848
Mariano Vazquez <mariano.vazquez@live.com>
2019-09-28 02:20:49 +0100

    code restructure

--- currency_converter.py
+++ currency_converter.py
@@ -9,12 +9,6 @@ from selenium.webdriver.support.ui import Select
 import os
 import random
 
-#Options for Local box
-chrome_options = webdriver.ChromeOptions()
-chrome_driver_path = '/Users/mariano/Desktop/scrapers/chromedriver'
-driver = webdriver.Chrome(chrome_driver_path, chrome_options=chrome_options)
-
-url = 'https://transferwise.com/ie/currency-converter/'
 
 def clear_amount_field():
     amount_field = driver.find_element_by_id("cc-amount")
@@ -23,16 +17,14 @@ def clear_amount_field():
 
 def currency_iteration(source_amount, url):
 
-    driver.get(url)
-
     source_c = ['USD', 'GBP', 'ARS']
-    target_c = ['INR', 'AUD', 'MXN']
+    target_c = ['INR', 'AUD', 'EUR']
 
     for value_a in sorted(source_c,key=lambda _: random.random()):
         for value_b in sorted(target_c,key=lambda _: random.random()):
-            wait = WebDriverWait(driver, 10)
-            clear_amount_field()
+            wait = WebDriverWait(driver, 5)
             amount = wait.until(EC.visibility_of_element_located((By.ID, "cc-amount")))
+            clear_amount_field()
             amount.click()
             amount.send_keys(str(source_amount))
 
@@ -42,20 +34,37 @@ def currency_iteration(source_amount, url):
             select_target = Select(driver.find_element_by_id('targetCurrency'))
             select_target.select_by_value(value_b)
 
-            convert_btn = driver.find_element(By.ID,"convert").click()
+            convert_button = driver.find_element(By.ID,"convert").click()
 
             converted_currency = wait.until(EC.visibility_of_element_located((By.ID, "cc-amount-to")))
 
             converted_amount = driver.find_element_by_id("cc-amount-to").get_attribute('value')
 
-            print(str(source_amount) + " " + value_a + " are equal to " + str(converted_amount) + " " + value_b)
+            print(">> " + str(source_amount) + " " + value_a + " are equal to " + str(converted_amount) + " " + value_b)
 
             driver.back()
 
 
 if __name__ == '__main__':
 
+  #Options for Local box
+  chrome_options = webdriver.ChromeOptions()
+  chrome_driver_path = '/Users/mariano/Desktop/scrapers/chromedriver'
+  driver = webdriver.Chrome(chrome_driver_path, chrome_options=chrome_options)
+
+  url = 'https://transferwise.com/ie/currency-converter/'
+  driver.get(url)
+
   os.system('clear')
+
+  print('\n')
+  print('###############################################################')
+  print('       Currency converter script -  Mariano Vazquez, 2019'      )
+  print('###############################################################')
+  print('\n')
+
   source_amount = input("Enter the amount to calculate: ")
+  print("\nHere are the results of your search:")
+  print('\n')
   currency_iteration(source_amount, url)
   driver.close()
