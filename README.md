94d01c469580bfb8d7d74c6043f005da656c54e6
Mariano Vazquez <mariano.vazquez@live.com>
2019-09-28 22:01:04 +0100

    added more info to readme.md

--- README.md
+++ README.md
@@ -2,13 +2,26 @@
 
 Automation script developed in Python 3 with Selenium Webdriver.
 
-## Installation
+## Requirements
 
-You should be able to clone this repository or run the currency_converter.py file.
+Requirements (on your system)
+	1.	Chrome/Chromium Browser (https://www.google.com/chrome/browser)
+	2.	Chrome WebDriver (https://chromedriver.storage.googleapis.com/index.html)
+	3.	Python3 (https://www.python.org/downloads/)
+	4.	Virtualenv ($ pip3 install virtualenv) - Optional
+	5.	Selenium ($ pip3 install selenium)
 
-```bash
-git clone https://github.com/pyrish/currency_converter.git
-```
+## Steps to run in your local machine
+
+	1.	Firstly, clone the repository using the git shell 
+  $ git clone https://github.com/pyrish/currency_converter.git
+	2.	Go to the base directory of the project $ cd currency-converter
+	3.	Install the requirements for the project $ pip install -r requirements.txt
+	5.	Download the ChromeWebdriver (according to the version of your Chrome browser) and extract into the directory where you saved/cloned the currency_converter.py script.
+	6.	In the file "currency_converter.py" replace "CHROME_PATH" variable with the absolute path of the downloaded Chrome Webdriver.
+	7.	Run the script $ python3 currency_converter.py
+	8.	Enter the details asked (amount, source currency, target currency)
+	9.	Voila!
 
 ## Usage
 
--- currency_converter.py
+++ currency_converter.py
@@ -1,4 +1,14 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""Python script using Selenium Webdriver to calculate the
+    conversion rate of a given amount, selecting the source and
+    target currencies. All these variables are provided by the user.
+    All data has been validated accordingly"""
+
+__author__ = "Mariano Vazquez"
+__version__ = "1.0"
+__email__ = "mariano.vazquez@live.com"
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -6,65 +16,100 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
+import platform # For getting the operating system name
+import subprocess # For executing a shell command
 import os
-import random
 
+CHROME_PATH = '../chromedriver' # Path to folder where repo has been cloned or where Chromedriver has been downloaded to
+URL = 'https://transferwise.com/ie/currency-converter/'
+AMOUNT_FIELD = 'cc-amount'
+SOURCE_CURRENCY = 'sourceCurrency'
+TARGET_CURRENCY = 'targetCurrency'
+CONVERTED_CURRENCY = 'cc-amount-to'
+
+def clear_screen():
+    if platform.system().lower() == "windows":
+        command = "cls"
+    else:
+        command = "clear"
+    return(subprocess.call(command) == 0)
 
 def clear_amount_field():
-    amount_field = driver.find_element_by_id("cc-amount")
+    amount_field = driver.find_element_by_id(AMOUNT_FIELD)
     amount_field.clear()
 
+def extract_dropdown_items():
+    select = Select(driver.find_element_by_id(SOURCE_CURRENCY))
+    options = select.options
+    options_list = []
+    for option in options:
+        options_list.append(option.get_attribute("value"))
+    return(options_list)
 
-def currency_iteration(source_amount, url):
+def currency_iteration(url, source_amount, from_currency, to_currency):
+    wait = WebDriverWait(driver, 5)
+    amount = wait.until(EC.visibility_of_element_located((By.ID, AMOUNT_FIELD)))
+    clear_amount_field()
+    amount.click()
+    amount.send_keys(str(source_amount))
 
-    source_c = ['USD', 'GBP', 'ARS']
-    target_c = ['INR', 'AUD', 'EUR']
+    select_source = Select(driver.find_element_by_id(SOURCE_CURRENCY))
+    select_source.select_by_value(from_currency)
 
-    for value_a in sorted(source_c,key=lambda _: random.random()):
-        for value_b in sorted(target_c,key=lambda _: random.random()):
-            wait = WebDriverWait(driver, 5)
-            amount = wait.until(EC.visibility_of_element_located((By.ID, "cc-amount")))
-            clear_amount_field()
-            amount.click()
-            amount.send_keys(str(source_amount))
+    select_target = Select(driver.find_element_by_id(TARGET_CURRENCY))
+    select_target.select_by_value(to_currency)
 
-            select_source = Select(driver.find_element_by_id('sourceCurrency'))
-            select_source.select_by_value(value_a)
+    convert_button = driver.find_element(By.ID,"convert").click()
+    converted_currency = wait.until(EC.visibility_of_element_located((By.ID, CONVERTED_CURRENCY)))
+    converted_amount = driver.find_element_by_id(CONVERTED_CURRENCY).get_attribute('value')
+    driver.back()
 
-            select_target = Select(driver.find_element_by_id('targetCurrency'))
-            select_target.select_by_value(value_b)
-
-            convert_button = driver.find_element(By.ID,"convert").click()
-
-            converted_currency = wait.until(EC.visibility_of_element_located((By.ID, "cc-amount-to")))
-
-            converted_amount = driver.find_element_by_id("cc-amount-to").get_attribute('value')
-
-            print(">> " + str(source_amount) + " " + value_a + " are equal to " + str(converted_amount) + " " + value_b)
-
-            driver.back()
+    return(float(converted_amount))
 
 
 if __name__ == '__main__':
 
   #Options for Local box
   chrome_options = webdriver.ChromeOptions()
-  chrome_driver_path = '/Users/mariano/Desktop/scrapers/chromedriver'
+  # chrome_driver_path = '/Users/mariano/Desktop/scrapers/chromedriver'
+  chrome_driver_path = CHROME_PATH
   driver = webdriver.Chrome(chrome_driver_path, chrome_options=chrome_options)
 
-  url = 'https://transferwise.com/ie/currency-converter/'
-  driver.get(url)
+  driver.get(URL)
 
-  os.system('clear')
+  # os.system('clear')
+  clear_screen()
 
   print('\n')
   print('###############################################################')
-  print('       Currency converter script -  Mariano Vazquez, 2019'      )
+  print('          Currency converter -  Mariano Vazquez, 2019'          )
   print('###############################################################')
   print('\n')
-
-  source_amount = input("Enter the amount to calculate: ")
-  print("\nHere are the results of your search:")
+  print("Popular currencies: EUR | GBP | USD | INR | CAD | JPY | AUD | CHF")
   print('\n')
-  currency_iteration(source_amount, url)
-  driver.close()
+
+  currency_list = extract_dropdown_items()
+
+  for i in range(1,6):
+
+      source_amount = float(input("Please enter the amount #" + str(i) + " to be converted: "))
+      while source_amount <= 0 or not source_amount:
+          source_amount = float(input("Amount should be a positive number greater than zero, please enter again the amount #" + str(i) + " to be converted: "))
+
+      from_currency = input("Please enter the source currency: ")
+      while from_currency.upper().strip() not in currency_list:
+          from_currency = input("Invalid source currency, please enter a valid source currency: ").strip()
+
+      to_currency = input("Please enter the target currency: ").strip()
+
+      while to_currency.upper().strip() not in currency_list:
+          to_currency = input("Invalid target currency, please enter a valid target currency: ").strip()
+
+      converted_amount = currency_iteration(URL, source_amount, from_currency.upper(), to_currency.upper())
+
+      print(">> " + str(source_amount) + " " + from_currency.upper() + " are equal to " + str(round(converted_amount, 2)) + " " + to_currency.upper())
+      print('\n')
+
+print('Thank you for using the currency converted tool. Enjoy your day, Bye!')
+print('\n')
+driver.close()
